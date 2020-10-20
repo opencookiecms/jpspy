6e93e21f670248c6d01d91d6cf96cccc416feb0a
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2020-10-20 17:57:15 +0800

    update model

---
+++ src/jpskit/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Kontraktor(models.Model):
+    pass
+
+class Projek:
+    pass
+
+class MR1:
+    pass
+
+class MR2:
+    pass
+
+class MR3:
+    pass
