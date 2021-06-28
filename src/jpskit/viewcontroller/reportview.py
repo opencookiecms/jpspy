31a2c95f24b61d75ed900a5f422ffc376a22323c
Afiq <syed.m.afiq@outlook.com>
2021-06-28 23:41:25 +0800

    test

--- src/jpskit/viewcontroller/reportview.py
+++ src/jpskit/viewcontroller/reportview.py
@@ -1,5 +1,5 @@
 """pip uninstall pdfminer" and "pip uninstall pdfminer.six".
-Then all clear, "pip install pdfminer.six"."""
+Then all clear, "pip install pdfminer.six". selamat boleh install"""
 
 from django.shortcuts import render, get_list_or_404, redirect, reverse
 from django.core.files.storage import FileSystemStorage
